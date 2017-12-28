78a8ccc4eedf374c5996bd7a61c9b4048b038fe7
Curia Adrian <adrian.curia@metrosystems.net>
2017-12-28 10:11:27 +0200

    first

---
+++ src/main/java/com/dbbyte/learn/SbJunitApplication.java
@@ -0,0 +1,12 @@
+package com.dbbyte.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJunitApplication.class, args);
+	}
+}
